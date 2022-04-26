40dd3ba102edd6df8801461423ab35a2e977c8e0
OB11TO <OB11TO61@yandex.ru>
2022-04-26 10:35:53 +0300

    Spring Initializr

---
+++ src/test/java/ru/ae/coursemodel/CourseModelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ae.coursemodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
